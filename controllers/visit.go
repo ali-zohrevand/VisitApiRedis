cae2c1d14ea28af491bfc17e1c6118228df038e5
alizohrevand <alihooshyar1990@gmail.com>
2019-09-10 18:57:23 +0430

    add docker compose

--- controllers/visit.go
+++ controllers/visit.go
@@ -7,6 +7,7 @@ import (
 
 func Visit(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	cash.VisitHappened()
-	number,_:=cash.VisitGetNumber()
+	number, _ := cash.VisitGetNumber()
 	w.Write([]byte(number))
+	panic("test")
 }
